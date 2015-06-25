a0442741351c0bac9d515f92d0205539acbce533
Keith S Messina <keith.s.messina@gmail.com>
2015-06-25 00:38:14 +0000

    Added some function.

--- Assignment-3.R
+++ Assignment-3.R
@@ -1,8 +1,10 @@
 best <- function(state, outcome) {
   ## Read outcome data
-  
+  outcome.data <- read.csv("outcome-of-care-measures.csv")
+  str(outcome.data[outcome.data==state])
   ## Check that state and outcome are valid
-  if(state!="AR"||)
+  outcome.filtered.state <- tryCatch(outcome.data$State[outcome.data$State==state],warning=function(){stop("invalid state")})
+  if(outcome!="heart attack"|outcome!="heart failure"|outcome!="pneumonia")stop("invalid outcome")
   
   ## Return hospital name in that state with lowest 30-day death
   ## rate
